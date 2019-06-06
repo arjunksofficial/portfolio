53239956859f454f32b37c1ec9f43e0668b6cd4e
Arjun Soman <arjunsoman@qburst.com>
2019-06-06 18:16:12 +0530

    Initialise project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Arjun K S</h1>
+        <h3>Software Engineer</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
