ee5c8ccb8c094dce37d34ce0bbda2f5ad98f8402
arjunks <arjunks@sellerapp.com>
2020-09-01 11:03:04 +0530

    added background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import bgimage from './assets/images/nandhi_hills.JPG';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Arjun K S</h1>
-        <h3>Software Engineer</h3>
+        <h3>Golang Backend Developer</h3>
       </header>
     </div>
   );
