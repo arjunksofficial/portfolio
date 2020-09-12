acc890718df1b1d7d4b36916188c14491073d0e2
arjunks <arjunks@sellerapp.com>
2020-09-12 22:03:17 +0530

    moved copying build folder logic to nginx folder

--- deploy/push_image.sh
+++ deploy/push_image.sh
@@ -1,5 +1,4 @@
 echo $GCLOUD_SERVICE_KEY > gcloud-service-key.json
-COPY --from=builder build/* /usr/share/nginx/html
 docker login -u _json_key --password-stdin https://us.gcr.io < gcloud-service-key.json
 docker build -f deploy/Dockerfile -t $CONTAINER_IMAGE .
 docker tag $CONTAINER_IMAGE "us.gcr.io/$GCLOUD_PROJECT_ID/$CONTAINER_IMAGE"
