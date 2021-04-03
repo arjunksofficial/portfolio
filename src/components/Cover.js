d4e0da38c1d475a85d06ab186906e2ae41376171
arjunksofficial <aluvaarjunks@gmail.com>
2021-04-03 12:00:14 +0530

    updated cover

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -18,7 +18,7 @@ function Cover() {
       <div className="CoverInfo">
         <h1>Arjun K S</h1>
         <h3>Golang Backend Developer</h3>
-        <p>Golang backend developer with more than 2 years experience. Experience with various DevOps terminologies and cloud services. Proven ability in optimizing web functionalities that improve data retrieval and workflow efficiencies.</p>
+        <p>Golang backend developer with over 3 years experience. Experience with various DevOps terminologies and cloud services. Proven ability in optimizing web functionalities that improve data retrieval and workflow efficiencies.</p>
         <Linkbar />
       </div>
     </div>
